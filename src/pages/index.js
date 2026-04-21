ded23c12de1791cccbc3c96732a68aa0137a5170
codervandan <dquintana.dev@gmail.com>
2026-04-21 11:00:19 -0700

    fixed styling and added the rest of the delete card functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,30 +34,9 @@ import Api from "../utils/Api.js";
 //   }
 // ];
 
-// INSTANTIATE CLASS
-const api = new Api({
-  baseUrl: "https://around-api.en.tripleten-services.com/v1/",
-  headers: {
-    authorization: "b305640e-0dfd-4073-a50a-45aaa9f07108", // Replace with your actual token
-    "Content-Type": "application/json"
-  }
-});
-
-api.getInitialCards()
-  .then((data) => {
-    console.log(data);
-    data.forEach(function (card) {
-      const cardElement = getCardElement(card);
-      cardsList.append(cardElement);
-    });
-  })
-  .catch(console.error);
-
-
-
-  // DOM SELECTORS
-  const editProfileBtn = document.querySelector(".profile__edit-btn")
-  const editProfileModal = document.querySelector("#edit-profile-modal");
+// DOM SELECTORS
+const editProfileBtn = document.querySelector(".profile__edit-btn")
+const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
 const editProfileFormEl = editProfileModal.querySelector(".modal__form");
@@ -84,17 +63,107 @@ const previewModalCloseBtn = previewModal.querySelector(".modal__close-btn");
 const previewImageEl = previewModal.querySelector(".modal__image");
 const previewNameEl = previewModal.querySelector(".modal__caption");
 
+const avatarEditBtn = document.querySelector(".profile__avatar-edit-btn");
+const avatarModal = document.querySelector("#edit-avatar-modal");
+const avatarForm = avatarModal.querySelector(".modal__form");
+const avatarInput = avatarModal.querySelector("#avatar-input");
+const avatarCloseBtn = avatarModal.querySelector(".modal__close-btn");
+const profileAvatarEl = document.querySelector(".profile__avatar");
+
+const deleteModal = document.querySelector("#delete-modal");
+const deleteForm = deleteModal.querySelector(".modal__form");
+const deleteCloseBtn = deleteModal.querySelector(".modal__close-btn");
+
+// INSTANTIATE CLASS
+const api = new Api({
+  baseUrl: "https://around-api.en.tripleten-services.com/v1/",
+  headers: {
+    authorization: "b305640e-0dfd-4073-a50a-45aaa9f07108", // Replace with your actual token
+    "Content-Type": "application/json"
+  }
+});
+
+Promise.all([api.getUserInfo(), api.getInitialCards()])
+  .then(([userData, cards]) => {
+    // set profile
+    profileNameEl.textContent = userData.name;
+    profileDescriptionEl.textContent = userData.about;
+
+    profileAvatarEl.src = userData.avatar;
+
+    // render cards
+    cards.forEach(card => {
+      const cardElement = getCardElement(card);
+      cardsList.append(cardElement);
+    });
+  })
+  .catch(console.error);
+
+
+// OPTIONAL POPUP
+
+
+function handleDeleteCard(cardElement, data) {
+  selectedCard = cardElement;
+  selectedCardId = data._id;
+  openModal(deleteModal);
+}
+
+function handleDeleteSubmit(evt) {
+  evt.preventDefault();
+
+  api.deleteCard(selectedCardId)
+    .then(() => {
+      selectedCard.remove();
+      closeModal(deleteModal);
+    })
+    .catch(console.error);
+  }
+
+
+avatarEditBtn.addEventListener("click", () => {
+  avatarInput.value = "";
+  openModal(avatarModal);
+});
+
+avatarCloseBtn.addEventListener("click", () => {
+  closeModal(avatarModal);
+});
+
+avatarForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  api.updateAvatar({
+    avatar: avatarInput.value
+  })
+  .then((userData) => {
+    profileAvatarEl.src = userData.avatar;
+    closeModal(avatarModal);
+  })
+  .catch(console.error);
+});
+
+
+
 editProfileFormEl.addEventListener("submit", handleProfileFormSubmit);
+
+let selectedCard;
+let selectedCardId;
 
 newPostFormEl.addEventListener("submit", function(evt) {
   evt.preventDefault();
-  const inputValues = {name: cardCaptionInput.value,link: cardImageInput.value}
-  const cardElement = getCardElement(inputValues);
-  cardsList.prepend(cardElement);
-  evt.target.reset();
-  // toggleButtonState();
-  disableButton(cardSubmitBtn, settings);
-  closeModal(newPostModal);
+
+  api.addCard({
+    name: cardCaptionInput.value,
+    link: cardImageInput.value
+  })
+  .then((cardData) => {
+    const cardElement = getCardElement(cardData);
+    cardsList.prepend(cardElement);
+    evt.target.reset();
+    closeModal(newPostModal);
+  })
+  .catch(console.error);
 });
 
 // EVENT LISTENERS
@@ -127,6 +196,12 @@ previewModalCloseBtn.addEventListener("click", () => {
   previewModal.classList.toggle("modal_is-opened");
 })
 
+deleteForm.addEventListener("submit", handleDeleteSubmit);
+
+deleteCloseBtn.addEventListener("click", () => {
+  closeModal(deleteModal);
+});
+
 // FUNCTIONS
 function openModal(modal) {
   modal.classList.add("modal_is-opened")
@@ -138,11 +213,17 @@ function closeModal(modal) {
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  profileNameEl.textContent = editProfileNameInput.value;
-  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
 
-  editProfileModal.classList.remove("modal_is-opened");
-  newPostModal.classList.remove("modal_is-opened");
+  api.updateUserInfo({
+    name: editProfileNameInput.value,
+    about: editProfileDescriptionInput.value
+  })
+  .then((userData) => {
+    profileNameEl.textContent = userData.name;
+    profileDescriptionEl.textContent = userData.about;
+    closeModal(editProfileModal);
+  })
+  .catch(console.error);
 }
 
 
@@ -156,16 +237,41 @@ function getCardElement(data) {
 
   cardTitleEl.textContent = data.name;
 
-  cardLikeBtnEl.addEventListener("click", () => {
-    cardLikeBtnEl.classList.toggle("card__like-btn_active")
-  });
+  // cardLikeBtnEl.addEventListener("click", () => {
+  //   cardLikeBtnEl.classList.toggle("card__like-btn_active")
+  // });
 
-  cardDeleteBtnEl.addEventListener("click", () => {
-    // cardDeleteBtnEl.classList.toggle("card__delete-btn_active");
-    // cardDeleteBtnEl.closest(".card").remove(); <-- You can do this method or the cardElement.remove() as well both will work
-    cardElement.remove();
-    // cardElement = null;
-  });
+  cardLikeBtnEl.addEventListener("click", () => {
+  const isLiked = cardLikeBtnEl.classList.contains("card__like-btn_active");
+
+  const request = isLiked
+    ? api.unlikeCard(data._id)
+    : api.likeCard(data._id);
+
+  request
+    .then((updatedCard) => {
+      cardLikeBtnEl.classList.toggle("card__like-btn_active");
+    })
+    .catch(console.error);
+});
+
+  // cardDeleteBtnEl.addEventListener("click", () => {
+  //   // cardDeleteBtnEl.classList.toggle("card__delete-btn_active");
+  //   // cardDeleteBtnEl.closest(".card").remove(); <-- You can do this method or the cardElement.remove() as well both will work
+  //   cardElement.remove();
+  //   // cardElement = null;
+  // });
+
+//   cardDeleteBtnEl.addEventListener("click", () => {
+//   api.deleteCard(data._id)
+//     .then(() => {
+//       cardElement.remove();
+//     })
+//     .catch(console.error);
+// });
+cardDeleteBtnEl.addEventListener("click", () => {
+  handleDeleteCard(cardElement, data);
+});
 
   cardImageEl.src = data.link;
   cardImageEl.alt = data.name;
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -4,6 +4,42 @@ class Api {
     this._headers = headers;
   }
 
+
+  _handleServerResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
+  // other methods for working with the API
+
+  // USER ROUTES
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers
+    }).then(this._handleServerResponse);
+  }
+
+  updateUserInfo({ name, about }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({ name, about })
+    }).then(this._handleServerResponse);
+  }
+
+  updateAvatar({ avatar }) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({ avatar })
+    }).then(this._handleServerResponse);
+  }
+
+  // CARD ROUTES
+
   getInitialCards() {
     // return fetch("https://around-api.en.tripleten-services.com/v1/cards", {
     //   headers: {
@@ -15,14 +51,39 @@ class Api {
     }).then(this._handleServerResponse);
   }
 
-  _handleServerResponse(res) {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Error: ${res.status}`);
+  addCard({ name, link }) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({ name, link })
+    }).then(this._handleServerResponse);
   }
 
-  // other methods for working with the API
+// DELETE CARDS
+
+  deleteCard( cardId ) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers
+    }).then(this._handleServerResponse);
+  }
+
+// LIKE/UNLIKE CARDS
+
+  likeCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: "PUT",
+      headers: this._headers
+    }).then(this._handleServerResponse);
+  }
+
+  unlikeCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
+      method: "DELETE",
+      headers: this._headers
+    }).then(this._handleServerResponse);
+  }
+
 }
 
 export default Api;
